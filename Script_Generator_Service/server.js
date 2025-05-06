119ce92838c47312013acfd37dde7f2b5f9043b7
Thuỳ Uyên <nguyenthithuyuyen1922@gmail.com>
2025-05-06 20:14:01 +0700

    add cors

--- Script_Generator_Service/server.js
+++ Script_Generator_Service/server.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const scriptRoutes = require('./src/routes/scriptRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://your-frontend-domain.com'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}))
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
